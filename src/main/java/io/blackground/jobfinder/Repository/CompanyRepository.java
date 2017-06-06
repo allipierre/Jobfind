e38b0955343953b52eb8f2b96c8ae25ef262e63d
allipierre <yottiallipierre40@yahoo.fr>
2017-06-06 10:29:41 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
+++ src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
@@ -10,14 +10,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import io.blackground.jobfinder.models.Company;
 
-
 /**
  * @author yotti
  *
  */
 
 public interface CompanyRepository extends CrudRepository<Company, Integer> {
-	
-    @Query("select * from Company")
-	  List<Company> allCompany();
+
+	@Query("select id,city,industry,location,name,numbere,website from Company")
+	List<Company> allCompany();
 }
