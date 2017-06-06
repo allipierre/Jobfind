35ea4ba0d99528a3481fc4c37338cb86ce8c3008
allipierre <yottiallipierre40@yahoo.fr>
2017-06-06 17:12:47 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/models/Company.java
+++ src/main/java/io/blackground/jobfinder/models/Company.java
@@ -23,7 +23,7 @@ import javax.persistence.OneToOne;
 @Getter
 @Setter
 @Entity
-@NamedNativeQuery(name = "Company.findByidIs", query = "SELECT id,name,website,about,city,location,industry,numbere FROM Company t", resultClass = Company.class)
+@NamedNativeQuery(name = "Company.findByidIs", query = "SELECT id,name,website,about,city,location,industry_id,numbere FROM Company t", resultClass = Company.class)
 public class Company {
 
 	@Id
