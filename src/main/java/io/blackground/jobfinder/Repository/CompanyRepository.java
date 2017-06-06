34e882cb1c65c1d0a4b3f3251d0d43f7c9e6a27b
allipierre <yottiallipierre40@yahoo.fr>
2017-06-06 15:44:13 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
+++ src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
@@ -22,14 +22,8 @@ public interface CompanyRepository extends CrudRepository<Company, Integer> {
 	@Query(nativeQuery = true)
 	public List<Company> findByidIs();
 
-	@Query(value = "SELECT id,name,website,about,city,location,industry,numbere FROM Company t", nativeQuery = true)
-//	@Query(value = "select i.name as industry,c.id"
-//			+ " id,c.website as website, c.about as about,"
-//			+ " c.city as city, c.location as location"
-//			+ ",c.numbere as numbere from industry i "
-//			+ "inner join company c "
-//			+ "on "
-//			+ "i.id=c.industry", nativeQuery = true)
+//	@Query(value = "SELECT id,name,website,about,city,location,industry,numbere FROM Company t", nativeQuery = true)
+	@Query(value = "select i.name as industry,c.id id,c.website as website, c.about as about, c.city as city, c.location as location,c.numbere as numbere, c.name as name from industry i inner join company c on i.id=c.industry", nativeQuery = true)
 	public List<Company> findJoin();
 	
 	
