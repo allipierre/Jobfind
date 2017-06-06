1319a42ed21673494c833cb069657800f87c0ac8
allipierre <yottiallipierre40@yahoo.fr>
2017-06-06 17:55:10 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/models/Company.java
+++ src/main/java/io/blackground/jobfinder/models/Company.java
@@ -7,10 +7,7 @@ package io.blackground.jobfinder.models;
  * @author yotti
  *
  */
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,10 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedNativeQuery;
-import javax.persistence.OneToOne;
 
-@Getter
-@Setter
 @Entity
 @NamedNativeQuery(name = "Company.findByidIs", query = "SELECT id,name,website,about,city,location,industry_id,numbere FROM Company t", resultClass = Company.class)
 public class Company {
@@ -38,10 +32,144 @@ public class Company {
 	private int industry_id;
 
 	private int numbere;
-
-	@ManyToOne
-    @JoinColumn(name = "industry_id")
 	private Industry industry;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the website
+	 */
+	public String getWebsite() {
+		return website;
+	}
+
+	/**
+	 * @param website
+	 *            the website to set
+	 */
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	/**
+	 * @return the about
+	 */
+	public String getAbout() {
+		return about;
+	}
+
+	/**
+	 * @param about
+	 *            the about to set
+	 */
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *            the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the location
+	 */
+	public int getLocation() {
+		return location;
+	}
+
+	/**
+	 * @param location
+	 *            the location to set
+	 */
+	public void setLocation(int location) {
+		this.location = location;
+	}
+
+	/**
+	 * @return the industry_id
+	 */
+	public int getIndustry_id() {
+		return industry_id;
+	}
+
+	/**
+	 * @param industry_id
+	 *            the industry_id to set
+	 */
+	public void setIndustry_id(int industry_id) {
+		this.industry_id = industry_id;
+	}
+
+	/**
+	 * @return the numbere
+	 */
+	public int getNumbere() {
+		return numbere;
+	}
+
+	/**
+	 * @param numbere
+	 *            the numbere to set
+	 */
+	public void setNumbere(int numbere) {
+		this.numbere = numbere;
+	}
+
+	/**
+	 * @return the industry
+	 */
+	@ManyToOne
+	@JoinColumn(name = "industry_id")
+	public Industry getIndustry() {
+		return industry;
+	}
+
+	/**
+	 * @param industry
+	 *            the industry to set
+	 */
+	public void setIndustry(Industry industry) {
+		this.industry = industry;
+	}
 
 	// private byte[] logo;
 
--- src/main/java/io/blackground/jobfinder/models/Industry.java
+++ src/main/java/io/blackground/jobfinder/models/Industry.java
@@ -34,8 +34,55 @@ public class Industry {
 
 	private String name;
 	
+	
+	private Set<Company> company;
+	
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the company
+	 */
 	@OneToMany(mappedBy = "industry", cascade = CascadeType.ALL)
-	private List<Company> company;
+	public Set<Company> getCompany() {
+		return company;
+	}
+
+	/**
+	 * @param company the company to set
+	 */
+	
+	public void setCompany(Set<Company> company) {
+		this.company = company;
+	}
+	
+	
 	
 
 }
