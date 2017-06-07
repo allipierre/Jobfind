ed185038e68728a02862b5e33ba6ab9247cf8b86
allipierre <yottiallipierre40@yahoo.fr>
2017-06-07 10:03:23 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
+++ src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
@@ -3,11 +3,10 @@
  */
 package io.blackground.jobfinder.Repository;
 
-import java.util.List;
 
 
 
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
 
 import io.blackground.jobfinder.models.Company;
