9abac041cf100db1ffd84f225fe25c6f861ba51b
allipierre <yottiallipierre40@yahoo.fr>
2017-06-07 11:15:30 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
+++ src/main/java/io/blackground/jobfinder/Repository/CompanyRepository.java
@@ -7,6 +7,9 @@ package io.blackground.jobfinder.Repository;
 
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import io.blackground.jobfinder.models.Company;
@@ -26,7 +29,8 @@ public interface CompanyRepository extends CrudRepository<Company, Integer> {
 	//@Query(value = "select i.name,c.id,c.website,c.about,c.city,c.location,c.numbere,c.name  from Industry i inner join Company c on i.id=c.industry_id", nativeQuery = true)
 	
 	//@Query(value = "SELECT id,name,website,about,city,location,industry,numbere FROM Company t", nativeQuery = true)//@Query(value = "Select * from Company a  left join Industry b on a.industry_id=b.id", nativeQuery = true)
-	//public List<Company> findJoin();
+	@Query(value = "SELECT companyId,companyName,website,about,city,location,industryid,numbere FROM Company", nativeQuery = true)
+	public List<Company> findJoin();
 	
 	
 
