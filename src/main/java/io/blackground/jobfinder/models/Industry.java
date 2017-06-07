fc0e0260327dbc939027c4e2ba22f88aaeee169f
allipierre <yottiallipierre40@yahoo.fr>
2017-06-07 10:23:28 +0200

    commit 12

--- src/main/java/io/blackground/jobfinder/models/Industry.java
+++ src/main/java/io/blackground/jobfinder/models/Industry.java
@@ -62,7 +62,7 @@ public class Industry {
 	/**
 	 * @return the company
 	 */
-	@OneToMany(mappedBy = "industry", targetEntity = Industry.class, fetch = FetchType.EAGER)
+	@OneToMany(mappedBy = "industry", targetEntity = Company.class, fetch = FetchType.EAGER)
 	public Set<Company> getCompany() {
 		return company;
 	}
