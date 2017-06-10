f4fbaf38b9d155a4f686b1c28485eb4266dd09cf
allipierre <yottiallipierre40@yahoo.fr>
2017-06-10 14:59:48 +0200

    commit 154

--- src/main/java/io/blackground/jobfinder/services/WebSecurityConfig.java
+++ src/main/java/io/blackground/jobfinder/services/WebSecurityConfig.java
@@ -30,9 +30,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/static/css/**", "/static/js/**").permitAll()
 		.antMatchers("/static/images/**").permitAll()
-				.antMatchers("/resources/**", "/registration","/index").permitAll().anyRequest().authenticated().and()
-				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+				.antMatchers("/resources/**", "/registration").permitAll().anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().csrf().ignoringAntMatchers("/index").and().logout().permitAll();
 	}
+	
+	
 
 	
 	@Autowired
