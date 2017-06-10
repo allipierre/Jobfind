26db881605d5600bfca7e6c9efc4dab1c7d330b0
allipierre <yottiallipierre40@yahoo.fr>
2017-06-10 13:05:07 +0200

    commit 15

--- src/main/java/io/blackground/jobfinder/services/WebSecurityConfig.java
+++ src/main/java/io/blackground/jobfinder/services/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.//csrf().disable()
                 authorizeRequests()
-                    .antMatchers("/resources/**", "/registration","/login","/header","/footer").permitAll()
+                    .antMatchers("/resources/**", "/registration","/login","/header","https://jobfind-master.herokuapp.com/static/js/site.js").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
