8901a524c64ac54cb58b1ba2018020c67fc736ac
allipierre <yottiallipierre40@yahoo.fr>
2017-06-13 20:04:43 +0200

    ww

--- src/main/java/io/blackground/jobfinder/utils/HibernateUtil.java
+++ src/main/java/io/blackground/jobfinder/utils/HibernateUtil.java
@@ -4,6 +4,7 @@
 package io.blackground.jobfinder.utils;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -16,13 +17,10 @@ public class HibernateUtil {
 
     private static SessionFactory sessionFactory;
 
-    static{
-        try {
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable e) {
-            throw new ExceptionInInitializerError(e);
-        }
-    }
+    Configuration configuration = new Configuration().configure();
+    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+    applySettings(configuration.getProperties());
+    SessionFactory factory = configuration.buildSessionFactory(builder.build());
 
     public static SessionFactory getSessionFactory(){
         return sessionFactory;
