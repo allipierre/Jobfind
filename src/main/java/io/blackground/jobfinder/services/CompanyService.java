ce6acec6ba20d6c9123aa05e6171f00da31bfc45
allipierre <yottiallipierre40@yahoo.fr>
2017-06-13 21:26:41 +0200

    ww

--- src/main/java/io/blackground/jobfinder/services/CompanyService.java
+++ src/main/java/io/blackground/jobfinder/services/CompanyService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import io.blackground.jobfinder.Repository.CompanyRepository;
 import io.blackground.jobfinder.models.Company;
 import io.blackground.jobfinder.models.User;
+import io.blackground.jobfinder.utils.HibernateUtil;
 
 /**
  * @author yotti
