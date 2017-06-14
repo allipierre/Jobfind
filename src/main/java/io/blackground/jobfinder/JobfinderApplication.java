22d1a4d6cc6e4b3b411991af9b59ff87f25c1151
allipierre <yottiallipierre40@yahoo.fr>
2017-06-14 08:17:29 +0200

    ww

--- src/main/java/io/blackground/jobfinder/JobfinderApplication.java
+++ src/main/java/io/blackground/jobfinder/JobfinderApplication.java
@@ -2,6 +2,8 @@ package io.blackground.jobfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
 public class JobfinderApplication {
@@ -9,4 +11,9 @@ public class JobfinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobfinderApplication.class, args);
 	}
+	
+	@Bean
+    public HibernateJpaSessionFactoryBean sessionFactory() {
+        return new HibernateJpaSessionFactoryBean();
+    }
 }
