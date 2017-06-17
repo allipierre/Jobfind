7c50bd1516508f9376ae069f90497c0a25bba91c
allipierre <yottiallipierre40@yahoo.fr>
2017-06-17 21:51:34 +0200

    aa

--- src/main/java/io/blackground/jobfinder/models/Industry.java
+++ src/main/java/io/blackground/jobfinder/models/Industry.java
@@ -18,62 +18,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Entity
+@Getter
+@Setter
 public class Industry {
 
+	@Id
+	@GeneratedValue
 	private long id;
 
 	private String industryName;
-
-	private Set<Company> company;
-
-	/**
-	 * @return the id
-	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	public long getId() {
-		return id;
-	}
-
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getIndustryName() {
-		return industryName;
-	}
-
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setIndustryName(String name) {
-		this.industryName = name;
-	}
-
-	/**
-	 * @return the company
-	 */
-	@OneToMany(mappedBy = "industry", targetEntity = Company.class, cascade = CascadeType.ALL)
-	public Set<Company> getCompany() {
-		return company;
-	}
-
-	/**
-	 * @param company
-	 *            the company to set
-	 */
-
-	public void setCompany(Set<Company> company) {
-		this.company = company;
-	}
-
 }
